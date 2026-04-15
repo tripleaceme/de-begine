c467d0769442316dad2b60967a12bd05342f6292
Adegbite Ayoade Abel <tripleaceme@gmail.com>
2026-04-16 00:41:20 +0100

    Update data quality thresholds for customers and orders

--- config/data_quality.py
+++ config/data_quality.py
@@ -2,26 +2,26 @@
 
 DATA_QUALITY_PROFILE = {
     "customers": {
-        "duplicate_rate": 0.3,
-        "missing_email_rate": 0.5,
-        "invalid_email_rate": 0.2,
-        "missing_city_rate": 0.5
+        "duplicate_rate": 0.4,
+        "missing_email_rate": 0.4,
+        "invalid_email_rate": 0.4,
+        "missing_city_rate": 0.4
     },
 
     "orders": {
-        "duplicate_rate": 0.3,
-        "missing_product_rate": 0.3,
-        "invalid_amount_rate": 0.3,
-        "negative_amount_rate": 0.5,
+        "duplicate_rate": 0.4,
+        "missing_product_rate": 0.4,
+        "invalid_amount_rate": 0.4,
+        "negative_amount_rate": 0.4,
         "invalid_status_rate": 0.4
     },
 
     "products": {
         "duplicate_rate": 0.3,
-        "missing_name_rate": 0.4,
-        "invalid_price_rate": 0.6,
-        "zero_price_rate": 0.3,
-        "category_mismatch_rate": 0.02,
-        "schema_drift_rate": 0.05
+        "missing_name_rate": 0.42,
+        "invalid_price_rate": 0.4,
+        "zero_price_rate": 0.4,
+        "category_mismatch_rate": 0.4,
+        "schema_drift_rate": 0.4
         }
 }
